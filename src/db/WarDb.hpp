540a2ec1af1841284f16437a2ce09bbfae922087
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-11-02 19:35:48 +0100

    Update war db schema

--- src/db/WarDb.hpp
+++ src/db/WarDb.hpp
@@ -5,46 +5,77 @@
 
 #include OATPP_CODEGEN_BEGIN(DbClient)
 
-class WarDb : public oatpp::orm::DbClient {
+class WarDb : public oatpp::orm::DbClient
+{
 public:
-    WarDb(const std::shared_ptr<oatpp::orm::Executor>& executor)
-        : oatpp::orm::DbClient(executor) {
-        oatpp::orm::SchemaMigration m(executor, "wars");
-        m.addFile(1, SQL_FILE_PATH"wars/init.sql");
-        m.migrate();
-    }
+	WarDb(const std::shared_ptr<oatpp::orm::Executor>& executor)
+		: DbClient(executor)
+	{
+		oatpp::orm::SchemaMigration m(executor, "wars");
+		m.addFile(1, SQL_FILE_PATH"wars/init.sql");
+		m.migrate();
+	}
 
-    QUERY(create,
-        "INSERT INTO wars (faction_id, enemy_faction_id, start_at, end_at, winner_id, "
-        "faction_score, faction_chain, enemy_faction_score, enemy_faction_chain) "
-        "VALUES (:w.faction_id, :w.enemy_faction_id, :w.start_at, :w.end_at, :w.winner_id, "
-        "COALESCE(:w.faction_score,0), COALESCE(:w.faction_chain,0), "
-        "COALESCE(:w.enemy_faction_score,0), COALESCE(:w.enemy_faction_chain,0)) "
-        "RETURNING *;",
-        PARAM(oatpp::Object<WarDto>, w))
+	QUERY(create,
+	      "INSERT INTO wars (faction_one_id, faction_two_id, start_at, end_at, winner_id, "
+	      "                  faction_one_score, faction_one_chain, faction_two_score, faction_two_chain) "
+	      "VALUES (:w.faction_one_id, :w.faction_two_id, :w.start_at, :w.end_at, :w.winner_id, "
+	      "        COALESCE(:w.faction_one_score,0), COALESCE(:w.faction_one_chain,0), "
+	      "        COALESCE(:w.faction_two_score,0), COALESCE(:w.faction_two_chain,0)) "
+	      "RETURNING *;",
+	      PARAM(oatpp::Object<WarDto>, w)
+	)
 
-    QUERY(update,
-        "UPDATE wars SET faction_id=:w.faction_id, enemy_faction_id=:w.enemy_faction_id, start_at=:w.start_at, "
-        "end_at=:w.end_at, winner_id=:w.winner_id, faction_score=:w.faction_score, faction_chain=:w.faction_chain, "
-        "enemy_faction_score=:w.enemy_faction_score, enemy_faction_chain=:w.enemy_faction_chain "
-        "WHERE id=:w.id RETURNING *;",
-        PARAM(oatpp::Object<WarDto>, w))
+	QUERY(update,
+	      "UPDATE wars SET "
+	      "  faction_one_id      = :w.faction_one_id, "
+	      "  faction_two_id      = :w.faction_two_id, "
+	      "  start_at            = :w.start_at, "
+	      "  end_at              = :w.end_at, "
+	      "  winner_id           = :w.winner_id, "
+	      "  faction_one_score   = :w.faction_one_score, "
+	      "  faction_one_chain   = :w.faction_one_chain, "
+	      "  faction_two_score   = :w.faction_two_score, "
+	      "  faction_two_chain   = :w.faction_two_chain "
+	      "WHERE id = :w.id "
+	      "RETURNING *;",
+	      PARAM(oatpp::Object<WarDto>, w)
+	)
 
-    QUERY(getById,
-        "SELECT * FROM wars WHERE id=:id;",
-        PARAM(oatpp::Int64, id))
+	QUERY(getById,
+	      "SELECT * FROM wars WHERE id = :id;",
+	      PARAM(oatpp::Int64, id)
+	)
 
-    QUERY(listWarsForFaction,
-        "SELECT * FROM wars "
-        "WHERE faction_id=:faction_id OR enemy_faction_id=:faction_id "
-        "ORDER BY start_at DESC LIMIT :limit OFFSET :offset;",
-        PARAM(oatpp::Int64, faction_id),
-        PARAM(oatpp::UInt32, limit),
-        PARAM(oatpp::UInt32, offset))
+	QUERY(getLatestWarForFaction,
+	      "SELECT * FROM wars "
+	      "WHERE faction_one_id = :faction_id OR faction_two_id = :faction_id "
+	      "ORDER BY start_at DESC, id DESC "
+	      "LIMIT 1;",
+	      PARAM(oatpp::Int64, faction_id)
+	)
 
-    QUERY(deleteById,
-        "DELETE FROM wars WHERE id=:id;",
-        PARAM(oatpp::Int64, id))
+	QUERY(deleteById,
+	      "DELETE FROM wars WHERE id = :id;",
+	      PARAM(oatpp::Int64, id)
+	)
+
+	QUERY(upsertById,
+	      "INSERT INTO wars (id, faction_one_id, faction_two_id, start_at, end_at, winner_id, "
+	      "                  faction_one_score, faction_one_chain, faction_two_score, faction_two_chain) "
+	      "VALUES (:w.id, :w.faction_one_id, :w.faction_two_id, :w.start_at, :w.end_at, :w.winner_id, "
+	      "        COALESCE(:w.faction_one_score,0), COALESCE(:w.faction_one_chain,0), "
+	      "        COALESCE(:w.faction_two_score,0), COALESCE(:w.faction_two_chain,0)) "
+	      "ON CONFLICT (id) DO UPDATE SET "
+	      "  end_at              = EXCLUDED.end_at, "
+	      "  winner_id           = EXCLUDED.winner_id, "
+	      "  faction_one_score   = COALESCE(EXCLUDED.faction_one_score, wars.faction_one_score), "
+	      "  faction_one_chain   = COALESCE(EXCLUDED.faction_one_chain, wars.faction_one_chain), "
+	      "  faction_two_score   = COALESCE(EXCLUDED.faction_two_score, wars.faction_two_score), "
+	      "  faction_two_chain   = COALESCE(EXCLUDED.faction_two_chain, wars.faction_two_chain) "
+	      "RETURNING *;",
+	      PARAM(oatpp::Object<WarDto>, w)
+	)
 };
 
 #include OATPP_CODEGEN_END(DbClient)
--- src/dto/WarDto.hpp
+++ src/dto/WarDto.hpp
@@ -2,22 +2,44 @@
 
 #include "oatpp/core/macro/codegen.hpp"
 #include "oatpp/core/Types.hpp"
+#include "torn_client/TornFactionWarResponseDto.hpp"
 
 #include OATPP_CODEGEN_BEGIN(DTO)
 
-class WarDto : public oatpp::DTO {
+class WarDto : public oatpp::DTO
+{
 	DTO_INIT(WarDto, DTO)
 
-		DTO_FIELD(Int64, id);
-	DTO_FIELD(Int64, factionId, "faction_id");
-	DTO_FIELD(Int64, enemyFactionId, "enemy_faction_id");
+	DTO_FIELD(Int64, id);
+	DTO_FIELD(Int64, factionOneId, "faction_one_id");
+	DTO_FIELD(Int64, factionTwoId, "faction_two_id");
 	DTO_FIELD(Int64, startAt, "start_at");
 	DTO_FIELD(Int64, endAt, "end_at");
 	DTO_FIELD(Int64, winnerId, "winner_id");
-	DTO_FIELD(Int32, factionScore, "faction_score");
-	DTO_FIELD(Int32, factionChain, "faction_chain");
-	DTO_FIELD(Int32, enemyFactionScore, "enemy_faction_score");
-	DTO_FIELD(Int32, enemyFactionChain, "enemy_faction_chain");
+	DTO_FIELD(Int32, factionOneScore, "faction_one_score");
+	DTO_FIELD(Int32, factionOneChain, "faction_one_chain");
+	DTO_FIELD(Int32, factionTwoScore, "faction_two_score");
+	DTO_FIELD(Int32, factionTwoChain, "faction_two_chain");
+
+	static oatpp::Object<WarDto> fromWarResponse(const oatpp::Object<TornFactionRankedWarDto>& warInfo)
+	{
+		auto war = createShared();
+		war->id = warInfo->war_id;
+		war->startAt = warInfo->start;
+		war->endAt = warInfo->end;
+		auto factionOne = warInfo->factions[0];
+		auto factionTwo = warInfo->factions[1];
+
+		war->factionOneId = factionOne->id;
+		war->factionOneScore = factionOne->score;
+		war->factionOneChain = factionOne->chain;
+
+		war->factionTwoId = factionTwo->id;
+		war->factionTwoScore = factionTwo->score;
+		war->factionTwoChain = factionTwo->chain;
+
+		return war;
+	}
 };
 
 #include OATPP_CODEGEN_END(DTO)
