a32a8d1f41f102936c4a8ad7b7c7134def1cd018
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-11-02 19:36:04 +0100

    Make torn_key nullable on users

--- src/db/sql/users/init.sql
+++ src/db/sql/users/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
   id           BIGSERIAL PRIMARY KEY,
-  torn_key     TEXT NOT NULL,
+  torn_key     TEXT,
   faction_id   BIGINT REFERENCES factions(id) ON DELETE SET NULL,
   created_at   BIGINT NOT NULL DEFAULT (EXTRACT(EPOCH FROM now()))::BIGINT
 );
