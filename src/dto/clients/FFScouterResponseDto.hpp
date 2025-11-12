5f21f0de988eabb6b7e5b8e877d8b8c28f70a68d
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-11-12 17:53:28 +0100

    Add ffscouter api client

---
+++ src/dto/clients/FFScouterResponseDto.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "oatpp/core/Types.hpp"
+#include "oatpp/core/macro/codegen.hpp"
+
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+class FFScouterItemDto : public oatpp::DTO {
+	DTO_INIT(FFScouterItemDto, DTO)
+
+		DTO_FIELD(Int64, player_id);
+	DTO_FIELD(Int64, bs_estimate);
+};
+
+#include OATPP_CODEGEN_END(DTO)
+
+using FFScouterResponseDto = oatpp::List<oatpp::Object<FFScouterItemDto>>;
---
+++ src/service/FFScouterApiService.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <optional>
+
+#include "AppConfig.hpp"
+#include "TornApiClient.hpp"
+#include "clients/FFScouterApiClient.hpp"
+#include "dto/clients/FFScouterResponseDto.hpp"
+#include "dto/clients/TornFactionMembersResponseDto.hpp"
+#include "dto/clients/TornFactionWarResponseDto.hpp"
+#include "oatpp/core/macro/component.hpp"
+#include "oatpp/web/client/ApiClient.hpp"
+
+
+class FFScouterApiService {
+	OATPP_COMPONENT(std::shared_ptr<FFScouterApiClient>, ffScouterApiClient);
+	OATPP_COMPONENT(std::shared_ptr<oatpp::data::mapping::ObjectMapper>, objectMapper);
+	OATPP_COMPONENT(std::shared_ptr<AppConfig>, appConfig);
+
+	using Status = oatpp::web::protocol::http::Status;
+public:
+	oatpp::async::CoroutineStarterForResult<const FFScouterResponseDto&> getScout(const std::vector<FactionMemberInfo>& memberInfo);
+};
